b70960569d7db4e25f85fcc7b636a65a9674bc87
Minhyeok Park <pmh_only@pmh.codes>
2023-01-04 00:21:06 +0900

    feat: add wakatime data fetcher

---
+++ src/fetchers/LangListFetcher.ts
@@ -0,0 +1,26 @@
+import { WAKATIME_LANGUAGE_STATICS_URL } from '../consts'
+import { WakatimeLangStaticsData } from '../entity/WakatimeLangStaticsData'
+import { DataFetcherBuilder } from './DataFetcherBuilder'
+import { IFetcher } from './IFetcher'
+
+export interface LangListData {
+  langs: Array<{
+    color: string
+    name: string
+    percent: number
+  }>
+}
+
+export class LangListFetcher implements IFetcher<LangListData> {
+  private readonly fetcher =
+    new DataFetcherBuilder<WakatimeLangStaticsData>()
+      .setUrl(WAKATIME_LANGUAGE_STATICS_URL)
+      .addHeader('Content-Type', 'application/json')
+      .build()
+
+  private readonly process = (data: WakatimeLangStaticsData): LangListData =>
+    ({ langs: data.data.sort((a, b) => b.percent - a.percent) })
+
+  public readonly fetch = async (): Promise<LangListData> =>
+    await this.fetcher.fetch().then(this.process)
+}
